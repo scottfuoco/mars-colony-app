a181abca130a722698047dc71827fe6842af2129
Stewart Fuoco <Stewart Fuoco>
2016-11-16 13:16:06 -0800

    save point

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NewColonist, Job } from '../models';
 import JobsService from '../services/jobs.service';
 
@@ -12,10 +13,12 @@ export class RegisterComponent implements OnInit {
 
   colonist: NewColonist;
   jobsList: Job[];
+ 
+  public registerForm = FormGroup;
 
   NO_JOB_SELECTED = '(none)';
 
-  constructor(jobsService: JobsService) {
+  constructor(jobsService: JobsService, private _fb: FormBuilder) {
     this.colonist = new NewColonist(null, null, this.NO_JOB_SELECTED);
     jobsService.getJobs().subscribe((jobs) => {
       this.jobsList = jobs;
@@ -25,9 +28,24 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit() {
+
   }
+
+  initAddress() {
+          // initialize our address
+          return this._fb.group({
+              street: ['', Validators.required],
+              postcode: ['']
+          });
+      }
 
   get jobSelected() {
     return this.colonist.job_id !== this.NO_JOB_SELECTED;
+  }
+
+  onSubmit(event, registerForm){
+    event.preventDefault();
+    console.log(registerForm);
+    registerForm.form.controls.age.invalid = true;
   }
 }
